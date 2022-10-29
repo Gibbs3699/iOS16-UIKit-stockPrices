c94d0b5629ee89fcf7fd98fcd786875fb39405bf
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-10-29 21:24:14 +0700

    init project

---
+++ Stock Prices/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stock Prices
+//
+//  Created by TheGIZzz on 29/10/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
