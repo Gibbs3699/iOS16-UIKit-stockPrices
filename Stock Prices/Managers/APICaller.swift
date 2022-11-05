0d4facdfa3d9ec9ce947151382d3e9480eb684c2
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-11-05 21:06:12 +0700

    add generic request function

--- Stock Prices/Managers/APICaller.swift
+++ Stock Prices/Managers/APICaller.swift
@@ -24,4 +24,29 @@ class APICaller {
     private func url(for endpoint: Endpoints, queryParam: [String: String] = [:]) -> URL? {
         return nil
     }
+    
+    private func request<T: Codable>(url: URL?, expecting: T.Type, completion: @escaping (Result<T,Error>) -> Void) {
+        guard let url = url else {
+            return completion(.failure(APIError.invalidURL))
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else {
+                if let error = error {
+                    completion(.failure(error))
+                } else {
+                    completion(.failure(APIError.noDataReturned))
+                }
+                return
+            }
+            
+            do {
+                let result = try JSONDecoder().decode(expecting, from: data)
+                completion(.success(result))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
 }
