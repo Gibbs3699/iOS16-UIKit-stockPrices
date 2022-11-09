81e41bb6a4ff76c9cb9effd531bd014f0f3fe079
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-11-09 22:55:34 +0700

    embedded search result to navigation item and update search result

--- Stock Prices/ViewControllers/WatchListViewController.swift
+++ Stock Prices/ViewControllers/WatchListViewController.swift
@@ -11,12 +11,23 @@ class WatchListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        view.backgroundColor = .systemBackground
         setupSearchController()
     }
 
     private func setupSearchController() {
-        
+        let resultVC = SearchResultsViewController()
+        let searchVC = UISearchController(searchResultsController: resultVC)
+        searchVC.searchResultsUpdater = self
+        navigationItem.searchController = searchVC
     }
 }
 
+extension WatchListViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text,
+        !query.trimmingCharacters(in: .whitespaces).isEmpty else { return }
+        
+        print(query)
+    }
+}
