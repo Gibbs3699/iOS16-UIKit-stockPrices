92658e6eb75c39dc537e3685c0286df8f865e146
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-11-10 23:23:56 +0700

    add uiview extension

---
+++ Stock Prices/Resources/Extension.swift
@@ -0,0 +1,36 @@
+//
+//  Extension.swift
+//  Stock Prices
+//
+//  Created by TheGIZzz on 10/11/2565 BE.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    var width: CGFloat {
+        frame.size.width
+    }
+    
+    var height: CGFloat {
+        frame.size.height
+    }
+    
+    var left: CGFloat {
+        frame.origin.x
+    }
+    
+    var right: CGFloat {
+        left + width
+    }
+    
+    var top: CGFloat {
+        frame.origin.y
+    }
+    
+    var bottom: CGFloat {
+        top + height
+    }
+}
